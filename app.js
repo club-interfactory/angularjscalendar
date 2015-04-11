bf638f7a77ae7a30bff5d5ab62731c270badf8f8
Susumu ISHIGAMI <susumu.ishigami@gmail.com>
2015-04-11 20:27:31 +0900

    スタイル調整

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
+var ajc_week_captions = ['日','月','火','水','木','金','土'];
+
 angular.module('app', [])
   .directive('calendarDaily', function() {
     return {
       restrict: 'E',
       require: '^ngModel',
+      replace: true,
       scope: {
-        day: '='
+        day: '=',
+        startDayofweek: '@'
       },
       controller: [ '$scope', function($scope) {
-        $scope.year = 2015;
-        $scope.month = 4;
+        var d = new Date();
+        $scope.year = d.getFullYear();
+        $scope.month = d.getMonth();
+        if ($scope.startDayofweek == undefined) {
+          $scope.startDayofweek = 0;
+        } else {
+          $scope.startDayofweek = parseInt($scope.startDayofweek);
+        }
+
+        $scope.dayofweeks = [];
+        for (var i = 0; i < 7; i++) {
+          var value = (i + $scope.startDayofweek) % 7;
+          $scope.dayofweeks[i] = {
+            caption: ajc_week_captions[value],
+            value: value
+          };
+        }
+
         $scope.refreshCalendar = function() {
-          $scope.calendar = [];
+          if ($scope.startDayofweek == undefined) {
+            $scope.startDayofweek = 0;
+          } else {
+            $scope.startDayofweek = parseInt($scope.startDayofweek);
+          }
+          $scope.calendar = {};
           var date = new Date($scope.year, $scope.month, 1);
-          date.setDate(date.getDate() - date.getDay());
+          date.setDate(date.getDate() - (date.getDay() + $scope.startDayofweek) % 7);
           $scope.calendar.weeks = [];
-          while (date.getMonth() <= $scope.month && date.getFullYear() <= $scope.year) {
+          while (100*date.getFullYear() + date.getMonth() <= 100 * $scope.year + $scope.month) {
             var days = [];
             for (var i = 0; i < 7; i++) {
               days.push({
                 otherMonth: date.getMonth() != $scope.month,
+                dayofweek: (i + $scope.startDayofweek) % 7,
                 caption   : date.getDate(),
                 value     : date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2) + ('0' + (date.getDate())).slice(-2)
               });
@@ -28,7 +54,6 @@ angular.module('app', [])
               days: days
             });
           }
-          console.log($scope.calendar);
         };
         $scope.nextMonth = function() {
           if ($scope.month == 11) {
